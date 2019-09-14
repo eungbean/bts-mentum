017b9d1de39307f43b8a54eaa34301d139a4df98
eungbean <eungbean@u.sogang.ac.kr>
2019-09-14 20:20:51 +0900

    #3.7 Making a To Do List part Three

---
+++ todo.js
@@ -0,0 +1,73 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const toDoS_LS = "toDos"
+let toDos = [];
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    todos = cleanToDos;
+    saveToDos();
+}
+
+function saveToDos(){
+    // Turn JSON into string and save it.
+    localStorage.setItem(toDoS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
+    const span = document.createElement("span");
+    const newID = toDos.length + 1;
+
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id = newID;
+    toDoList.appendChild(li);
+
+    // push todo in array
+    const toDoObj = {
+        text: text,
+        id: newID,
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos(){
+    const toDos = localStorage.getItem(toDoS_LS);
+    if (toDos !== null){
+        const parsedToDos = JSON.parse(toDos);
+        parsedToDos.forEach(
+            function(toDo){
+                paintToDo(toDo.text);
+            }
+        )
+    }
+}
+
+function init(){
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit)
+}
+
+init();
+
